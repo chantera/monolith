9164e080cd59aca81e17e98219233fc808190780
Hiroki Teranishi <teranishihiroki@gmail.com>
2018-03-26 02:42:29 +0900

    Fix.

--- examples/tagger/utils.rs
+++ examples/tagger/utils.rs
@@ -1,15 +1,15 @@
 use primitiv::{Device, devices};
 
-#[cfg(primitiv_cuda)]
+#[cfg(feature = "primitiv-cuda")]
 pub fn select_device(id: i32) -> Box<Device> {
     if id >= 0 {
-        Box::new(devices::CUDA::new(id))
+        Box::new(devices::CUDA::new(id as u32))
     } else {
         Box::new(devices::Naive::new())
     }
 }
 
-#[cfg(not(primitiv_cuda))]
+#[cfg(not(feature = "primitiv-cuda"))]
 #[allow(unused_variables)]
 pub fn select_device(id: i32) -> Box<Device> {
     Box::new(devices::Naive::new())
