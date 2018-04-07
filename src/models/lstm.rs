4a0359d6b4cdc730e69a2007968bf68cfb099227
Hiroki Teranishi <teranishihiroki@gmail.com>
2018-04-08 01:11:06 +0900

    Implement transpose_sequence for nodes.

--- src/models/lstm.rs
+++ src/models/lstm.rs
@@ -277,3 +277,35 @@ impl BiLSTM {
 }
 
 impl_model!(BiLSTM, model);
+
+pub fn transpose_sequence(xs: Vec<Node>) -> Vec<Node> {
+    let batch_size = xs[0].shape().batch() as usize;
+    let mut lengths = vec![xs.len(); batch_size];
+    let mut end = batch_size;
+    let xs: Vec<Node> = xs.into_iter()
+        .enumerate()
+        .map(|(i, x)| {
+            let mut s = x.shape();
+            let len = s.batch() as usize;
+            if len < end {
+                for l in lengths[len..end].iter_mut() {
+                    *l = i;
+                }
+                end = len;
+            }
+            let diff = batch_size - len;
+            if diff > 0 {
+                s.update_batch(diff as u32);
+                F::batch::concat(&[x, F::zeros(s)])
+            } else {
+                x
+            }
+        })
+        .collect();
+    let xs_transposed = F::batch::split(F::concat(&xs, 0), batch_size as u32);
+    xs_transposed
+        .into_iter()
+        .zip(lengths.into_iter())
+        .map(|(x, len)| F::slice(x, 0, 0, len as u32))
+        .collect()
+}
