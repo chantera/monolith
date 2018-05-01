6ca2acc6dd9e290981094cd7fd550781a9926cde
Hiroki Teranishi <teranishihiroki@gmail.com>
2018-05-02 05:35:39 +0900

    Separate logging modules.

---
+++ src/logging/app.rs
@@ -0,0 +1,111 @@
+use std::fs;
+use std::path::PathBuf;
+use std::io::Write;
+use std::thread;
+use std::time::Duration;
+
+use chrono::prelude::*;
+pub use slog::FilterLevel as Level;
+use slog::{Discard, Logger, Record};
+use uuid::{Uuid, NAMESPACE_OID as UUID_NAMESPACE_OID};
+
+use super::{Config, Error, create_logger_with_kv_and_time};
+
+#[derive(Debug)]
+pub struct AppLogger {
+    inner: Logger,
+    config: Config,
+    accessid: String,
+    accesstime: DateTime<Local>,
+    filepath: Option<PathBuf>,
+}
+
+impl AppLogger {
+    pub fn new<C: Into<Config>>(config: C) -> Result<Self, Error> {
+        let accesstime = Local::now();
+        let accessid = Uuid::new_v5(&UUID_NAMESPACE_OID, &accesstime.to_string()).to_string()[..8]
+            .to_string();
+        let c = config.into();
+
+        let (inner, filepath) = create_logger_with_kv_and_time(
+            c.clone(),
+            o!("accessid" => accessid.clone()),
+            &accesstime,
+        )?;
+        let mut logger = AppLogger {
+            inner: inner,
+            config: c,
+            accessid: accessid,
+            accesstime: accesstime,
+            filepath: filepath,
+        };
+        AppLogger::initialize(&mut logger);
+        Ok(logger)
+    }
+
+    fn initialize(&mut self) {
+        info!(
+            self,
+            "LOG Start with ACCESSID=[{}] ACCESSTIME=[{}]",
+            self.accessid,
+            self.accesstime.to_rfc3339(),
+        );
+    }
+
+    fn finalize(&mut self) {
+        let processtime = Local::now()
+            .signed_duration_since(self.accesstime)
+            .num_milliseconds() as f64 * 1e-3;
+        info!(
+            self,
+            "LOG End with ACCESSID=[{}] ACCESSTIME=[{}] PROCESSTIME=[{}]",
+            self.accessid,
+            self.accesstime.to_rfc3339(),
+            processtime,
+        );
+        self.inner = Logger::root(Discard, o!());
+        if let Some(ref path) = self.filepath {
+            thread::sleep(Duration::from_millis(1));
+            let result = fs::OpenOptions::new().append(true).open(path).map(
+                |mut file| {
+                    write!(file, "\n").map(|_| ()).and_then(|()| file.flush())
+                },
+            );
+            match result {
+                Ok(_) => {}
+                Err(e) => eprintln!("unable to write a newline: {}", e),
+            }
+        }
+    }
+
+    #[inline]
+    pub fn log(&self, record: &Record) {
+        self.inner.log(record);
+    }
+
+    pub fn get_inner(&self) -> &Logger {
+        &self.inner
+    }
+
+    pub fn config(&self) -> &Config {
+        &self.config
+    }
+
+    pub fn accessid(&self) -> &str {
+        &self.accessid
+    }
+
+    pub fn accesstime(&self) -> &DateTime<Local> {
+        &self.accesstime
+    }
+
+    pub fn create(&self) -> Logger {
+        self.inner.new(o!())
+    }
+}
+
+impl Drop for AppLogger {
+    fn drop(&mut self) {
+        AppLogger::finalize(self);
+    }
+}
--- src/logging/mod.rs
+++ src/logging/mod.rs
@@ -4,23 +4,21 @@ use std::fs::{self, File, OpenOptions};
 use std::path::{Path, PathBuf, MAIN_SEPARATOR};
 use std::io as std_io;
 use std::io::Write;
-#[cfg(feature = "app")]
-use std::thread;
-#[cfg(feature = "app")]
-use std::time::Duration;
 
 use chrono::prelude::*;
 pub use slog::FilterLevel as Level;
 use slog::{Discard, Drain, Duplicate, Fuse, LevelFilter, Level as LogLevel, Logger, OwnedKV,
            SendSyncRefUnwindSafeKV};
-#[cfg(feature = "app")]
-use slog::Record;
 use slog_async::Async;
 use slog_term::{CompactFormat, Decorator, FullFormat, PlainDecorator, TermDecorator};
-#[cfg(feature = "app")]
-use uuid::{Uuid, NAMESPACE_OID as UUID_NAMESPACE_OID};
 
 use utils;
+
+#[cfg(feature = "app")]
+pub use self::app::AppLogger;
+
+#[cfg(feature = "app")]
+mod app;
 
 #[derive(Debug)]
 pub enum Stream {
@@ -243,7 +241,7 @@ where
     create_logger_with_kv_and_time(config, values, &Local::now()).map(|(logger, _)| logger)
 }
 
-fn create_logger_with_kv_and_time<C: Into<Config>, T, Tz: TimeZone>(
+pub(crate) fn create_logger_with_kv_and_time<C: Into<Config>, T, Tz: TimeZone>(
     config: C,
     values: OwnedKV<T>,
     datetime: &DateTime<Tz>,
@@ -372,97 +370,5 @@ where
 
     } else {
         Ok(dir.join(format!("{}{}", stem, ext)))
-    }
-}
-
-#[cfg(feature = "app")]
-#[derive(Debug)]
-pub struct AppLogger {
-    inner: Logger,
-    config: Config,
-    accessid: String,
-    accesstime: DateTime<Local>,
-    filepath: Option<PathBuf>,
-}
-
-#[cfg(feature = "app")]
-impl AppLogger {
-    pub fn new<C: Into<Config>>(config: C) -> Result<Self, Error> {
-        let accesstime = Local::now();
-        let accessid = Uuid::new_v5(&UUID_NAMESPACE_OID, &accesstime.to_string()).to_string()[..8]
-            .to_string();
-        let c = config.into();
-
-        let (inner, filepath) = create_logger_with_kv_and_time(
-            c.clone(),
-            o!("accessid" => accessid.clone()),
-            &accesstime,
-        )?;
-        let mut logger = AppLogger {
-            inner: inner,
-            config: c,
-            accessid: accessid,
-            accesstime: accesstime,
-            filepath: filepath,
-        };
-        AppLogger::initialize(&mut logger);
-        Ok(logger)
-    }
-
-    fn initialize(&mut self) {
-        info!(
-            self,
-            "LOG Start with ACCESSID=[{}] ACCESSTIME=[{}]",
-            self.accessid,
-            self.accesstime.to_rfc3339(),
-        );
-    }
-
-    fn finalize(&mut self) {
-        let processtime = Local::now()
-            .signed_duration_since(self.accesstime)
-            .num_milliseconds() as f64 * 1e-3;
-        info!(
-            self,
-            "LOG End with ACCESSID=[{}] ACCESSTIME=[{}] PROCESSTIME=[{}]",
-            self.accessid,
-            self.accesstime.to_rfc3339(),
-            processtime,
-        );
-        self.inner = Logger::root(Discard, o!());
-        if let Some(ref path) = self.filepath {
-            thread::sleep(Duration::from_millis(1));
-            let result = OpenOptions::new().append(true).open(path).map(|mut file| {
-                write!(file, "\n").map(|_| ()).and_then(|()| file.flush())
-            });
-            match result {
-                Ok(_) => {}
-                Err(e) => eprintln!("unable to write a newline: {}", e),
-            }
-        }
-    }
-
-    #[inline]
-    pub fn log(&self, record: &Record) {
-        self.inner.log(record);
-    }
-
-    pub fn get_inner(&self) -> &Logger {
-        &self.inner
-    }
-
-    pub fn config(&self) -> &Config {
-        &self.config
-    }
-
-    pub fn create(&self) -> Logger {
-        self.inner.new(o!())
-    }
-}
-
-#[cfg(feature = "app")]
-impl Drop for AppLogger {
-    fn drop(&mut self) {
-        AppLogger::finalize(self);
     }
 }
